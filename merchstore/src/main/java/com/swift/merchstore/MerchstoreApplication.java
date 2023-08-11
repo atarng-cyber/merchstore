3e27a40308c9b39486f038a53e575baa96353236
Dylan Lim <112593644+DeadShinigamiXV@users.noreply.github.com>
2023-08-10 23:51:31 -0700

    Functioning Baseline

--- merchstore/src/main/java/com/swift/merchstore/MerchstoreApplication.java
+++ merchstore/src/main/java/com/swift/merchstore/MerchstoreApplication.java
@@ -1,17 +1,13 @@
 package com.swift.merchstore;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class MerchstoreApplication {
 
 	public static void main(String[] args) {
-		new SpringApplicationBuilder(MerchstoreApplication.class)
-				.web(WebApplicationType.NONE)
-				.run(args);
-
+		SpringApplication.run(MerchstoreApplication.class, args);
 	}
+
 }
