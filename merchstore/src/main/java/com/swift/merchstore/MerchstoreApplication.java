c497208d9f6afba77f31e09d6454a924df20272f
Dylan Lim <112593644+DeadShinigamiXV@users.noreply.github.com>
2023-08-10 23:32:21 -0700

    Downgraded springboot for version compatability errors

--- merchstore/src/main/java/com/swift/merchstore/MerchstoreApplication.java
+++ merchstore/src/main/java/com/swift/merchstore/MerchstoreApplication.java
@@ -1,14 +1,17 @@
 package com.swift.merchstore;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class MerchstoreApplication {
 
 	public static void main(String[] args) {
+		new SpringApplicationBuilder(MerchstoreApplication.class)
+				.web(WebApplicationType.NONE)
+				.run(args);
 
-		SpringApplication.run(MerchstoreApplication.class, args);
 	}
-
 }
