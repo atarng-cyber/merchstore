0f3285ca137b5a9ec367fc5425c42ff87cfe9751
Elias Alvarez <elias.alvarez880@gmail.com>
2023-08-13 02:49:23 -0700

    set up product manipulation with sql database - adding a product to a category does not work for now

---
+++ merchstore/src/main/java/com/swift/merchstore/dto/ProductDto.java
@@ -0,0 +1,65 @@
+package com.swift.merchstore.dto;
+
+import javax.validation.constraints.NotNull;
+
+public class ProductDto {
+    //id is optional for creation
+    //mandatory for update
+    private Integer id;
+    private @NotNull String name;
+    private @NotNull String imageURL;
+    private @NotNull double price;
+    private @NotNull String description;
+    private @NotNull Integer categoryId;
+
+    public ProductDto() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getImageURL() {
+        return imageURL;
+    }
+
+    public void setImageURL(String imageURL) {
+        this.imageURL = imageURL;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Integer getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(Integer categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+}
